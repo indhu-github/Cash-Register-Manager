3509e8b492f7d4fe90b00fb3023a377e501b2654
indhu-github <indhusworkmail@gmail.com>
2021-08-02 20:27:43 +0530

    functionality

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cashGiven = document.getElementById("cash-given");
 const checkButton = document.getElementById("check-button");
 const errorMessage = document.getElementById("error-message");
 
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+const notesCount = document.querySelectorAll(".count");
+
 checkButton.addEventListener("click", function validateBillAnsCashAmount() {
   errorMessage.style.display = "none";
   if (billAmount.value > 0) {
@@ -16,9 +19,18 @@ checkButton.addEventListener("click", function validateBillAnsCashAmount() {
   }
 });
 
+function calculateChange(amountToBeReturned) {
+  for (i in availableNotes) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned %= availableNotes[i];
+    notesCount[i].innerText = numberOfNotes;
+  }
+}
+
 function hideMessage() {
   errorMessage.style.display = "none";
 }
+
 function showMessage(message) {
   errorMessage.style.display = "block";
   errorMessage.innerText = message;
