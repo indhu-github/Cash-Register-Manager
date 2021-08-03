a236d7eb5982dbe5cc41ebbd86fd41a0d6be8dd2
indhu-github <indhusworkmail@gmail.com>
2021-08-03 19:31:10 +0530

    styling

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const billAmount = document.getElementById("bill-amount");
 const cashGiven = document.getElementById("cash-given");
 const checkButton = document.getElementById("check-button");
 const errorMessage = document.getElementById("error-message");
-
+const nextButton = document.getElementById("next-button");
 const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 const notesCount = document.querySelectorAll(".count");
 
-checkButton.addEventListener("click", function validateBillAnsCashAmount() {
+nextButton.addEventListener("click", function validateBillAmount() {
+  if (billAmount.value > 0) {
+    document.getElementById("cashGiven-div").style.visibility = "visible";
+  }
+});
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
   errorMessage.style.display = "none";
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
